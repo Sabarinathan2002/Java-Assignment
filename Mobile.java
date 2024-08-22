c97dab142b141449f7cda0716cb6151a703a52e4
SABARINATHAN M <117084504+Sabarinathan2002@users.noreply.github.com>
2024-08-22 18:53:28 +0530

    Add files via upload
    Week1 (Assignment 1)

---
+++ Mobile.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileName = "Redmi";
+		int mobilePrice = 45000;
+		long imeNumber = 4830438043830324l;
+		float mobileOffer = 5.00004f;
+		char mobileLogo = 'R';
+		boolean isDisplayed = true;
+		System.out.println(mobileName);
+		System.out.println(imeNumber);
+		System.out.println(mobileOffer);
+		System.out.println(mobileLogo);
+		System.out.println(isDisplayed);
+		System.out.println(mobileName + " " + mobilePrice + " " + imeNumber + " " + mobileOffer + " " + mobileLogo + " "
+				+ isDisplayed);
+		System.out.println(mobileName + " \n" + mobilePrice + " \n" + imeNumber + " \n" + mobileOffer + " \n"
+				+ mobileLogo + " \n" + isDisplayed);
+
+	}
+
+}
