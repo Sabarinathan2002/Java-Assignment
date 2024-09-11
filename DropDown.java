805690583e8fb6571ef89466d0ff34eefe5b4a66
SABARINATHAN M <117084504+Sabarinathan2002@users.noreply.github.com>
2024-09-11 23:26:11 +0530

    Add files via upload

---
+++ DropDown.java
@@ -0,0 +1,43 @@
+package week4.day3;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.leafground.com/select.xhtml");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		WebElement element = driver.findElement(By.xpath("//select[@class='ui-selectonemenu']"));
+		Select tools = new Select(element);
+		tools.selectByVisibleText("Selenium");
+		driver.findElement(By.xpath("//label[@id='j_idt87:country_label']")).click();
+		driver.findElement(By.xpath("//li[text()='India']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//label[@id='j_idt87:city_label']")).click();
+		driver.findElement(By.xpath("//li[text()='Chennai']")).click();
+		WebElement element2 = driver.findElement(By.xpath("//input[@id='j_idt87:auto-complete_input']"));
+		element2.sendKeys("Selenium WebDriver");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//li[@data-item-label='Selenium WebDriver']")).click();
+		element2.sendKeys("Playwright");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//li[@data-item-label='Playwright']")).click();
+		element2.sendKeys("PostMan");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//li[@data-item-label='PostMan']")).click();
+		driver.findElement(By.xpath("//label[@id='j_idt87:lang_label']")).click();
+		driver.findElement(By.xpath("//li[@id='j_idt87:lang_1']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//label[@id='j_idt87:value_label']")).click();
+		driver.findElement(By.xpath("//li[@id='j_idt87:value_2']")).click();
+
+	}
+
+}
